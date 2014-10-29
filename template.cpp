8a248511bf63f3a0806a7f1ae6b60f1ae3f13c09
Zhengqi Yang <zyang36@gelib-057-18.ews.illinois.edu>
2014-10-29 15:54:56 -0500

    Added template.cpp

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ifstream in("filename.in");
+
+	return 0;
+}
